a3276dc1c4b97478565205603c450e960a2626ef
Alexander Konovalov <shuryak@protonmail.com>
2021-07-18 12:20:24 +0300

    fix: other PDF density in pdf-creator.sh

--- .github/workflows/pdf-creator.sh
+++ .github/workflows/pdf-creator.sh
@@ -7,5 +7,5 @@ mv $var/*/*.jpg $var/images-to-pdf
 
 convert $var/*/*.png -background white -alpha remove -alpha off $var/images-to-pdf/image.png
 
-convert $var/images-to-pdf/* -page a4 -quality 100 ${var%?}.pdf
+convert $var/images-to-pdf/* -units pixelsperinch -density 300 -page a4 -quality 100 ${var%?}.pdf
 done
